d681c6830cbae8b919b3978e156771019eea6861
crazycoder119 <crazycoder119@gmail.com>
2021-01-20 10:29:09 +0530

    Java Basics

---
+++ hands on java/src/com/chandima/002/InputOutput/Application.java
@@ -0,0 +1,13 @@
+package com.chandima.InputOutput;
+
+import java.util.Scanner;
+
+public class Application {
+    public static void main(String args[]){
+        // System inputs
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        // System outputs
+        System.out.println("Your input is : " +s);
+    }
+}
---
+++ hands on java/src/com/chandima/DataTypes/Application.java
@@ -0,0 +1,14 @@
+package com.chandima.DataTypes;
+
+import java.util.Scanner;
+
+public class Application {
+    public static void main(String args[]){
+        System.out.println("https://www.geeksforgeeks.org/data-types-in-java/");
+        //Primitive data types
+        int x = 10;  //declaired and initialized
+        // Non Primitives use objects
+        String s ="Hello world";
+
+    }
+}
